f209837220cef2db817683805b7befc09b6d5b7b
Charan63833 <80826628+Charan63833@users.noreply.github.com>
2021-05-15 23:21:13 +0530

    15/05/21

---
+++ Employee/src/app/employee-mock.ts
@@ -0,0 +1,11 @@
+import { Employee} from './model/employee';
+export const EMPLOYEES: Employee[] =[
+{id :101, name :'charan',address:'nellore',age:21,company:'CTS'},
+{id :102, name :'ravi',address:'nellore',age:21,company:'CTS'},
+{id :103, name :'kalyan',address:'nellore',age:21,company:'CTS'},
+{id :104, name :'srika',address:'nellore',age:27,company:'CTS'},
+{id :105, name :'chinna',address:'nellore',age:29,company:'CTS'},
+
+
+
+];
